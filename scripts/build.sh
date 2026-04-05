b91c8f37ed5f0706f947a58a50b400b4bdcd3edc
Lampadoforo <federico.frassetto@tuta.io>
2026-04-05 11:00:00 +0200

    Fix scripts

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,16 +1,16 @@
 #!/bin/sh
 set -e
-npm install svgo @swc/cli @swc/core
+npm install svgo @swc/cli @swc/core > /dev/null
 favicon() {
-	sed "s_$1_data:image/svg+xml,$(npx svgo "$1" -o - | sed 's/"/%22/g;s/</%3C/g;s/>/%3E/g')_"
+	sed "s_${1#*/}_data:image/svg+xml,$(npx svgo "$1" -o - | sed 's/"/%22/g;s/#/%23/g;s/</%3C/g;s/>/%3E/g')_"
 }
-svg() {
+icons() {
 	for icon in "$@"; do
-		echo "$icon"
-		sed "s_<img alt=\"\" src=\"$icon\"/>_$(npx svgo "$icon" -o -)_"
+		e="${e}s_<img alt=\"\" src=\"${icon#*/}\"/>_$(npx svgo "$icon" -o -)_;"
 	done
+	sed "$e"
 }
 worker() {
-	sed "s_<script src=\"$1\"></script>__;s_'use strict';onmessage=\${onmessage}_$(npx swc -q "$1")_"
+	sed "s#<script src=\"${1#*/}\"></script>##;s#'use strict';onmessage=\${onmessage}#$(npx swc -q "$1")#"
 }
-tr -d '\t\n' < main.xhtml | favicon favicon.svg | svg icons/*.svg | worker worker.js > index.xhtml
+cat src/main.xhtml | favicon src/favicon.svg | icons src/icons/*.svg | worker src/worker.js > index.xhtml
--- scripts/check.sh
+++ scripts/check.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 set -e
-npm install shellcheck
+npm install shellcheck > /dev/null
 npx shellcheck scripts/*.sh
-npm install vnu-jar
+npm install vnu-jar > /dev/null
 npx vnu-jar --Werror --format text --also-check-css --also-check-svg src/*.xhtml src/*.css src/*.svg src/icons/*.svg
-npm install eslint @eslint/js @html-eslint/eslint-plugin
+npm install eslint @eslint/js @html-eslint/eslint-plugin > /dev/null
 npx eslint
