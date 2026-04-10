647e5ce6a96098996a7d1ef22e76442ca4ed243b
Lampadoforo <federico.frassetto@tuta.io>
2026-04-10 07:41:09 +0200

    Update build.sh

--- scripts/build.sh
+++ scripts/build.sh
@@ -24,6 +24,6 @@ worker() {
 }
 mkdir -p _site/
 favicon src/favicon.svg < src/main.xhtml | icons src/icons/*.svg | worker src/worker.js > _site/index.xhtml
-rsvg-convert --page-width 1200 --page-height 630 --top 15 --left 300 -w 600 -h 600 src/favicon.svg > og.png
+rsvg-convert --page-width 1200 --page-height 630 --top 15 --left 300 -w 600 -h 600 src/favicon.svg > _site/og.png
 npm install vnu-jar > /dev/null
 npx vnu-jar --Werror _site/index.xhtml
